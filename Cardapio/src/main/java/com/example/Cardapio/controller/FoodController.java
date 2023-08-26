64c26db9ec3566149351d083122f68c961647fb7
Silas Eduardo de Souza <silas.edu.bjg@gmail.com>
2023-08-26 16:09:00 -0300

    Criando projeto Spring react: Instalando dependencias e configuracoes de acesso ao banco.

---
+++ Cardapio/src/main/java/com/example/Cardapio/controller/FoodController.java
@@ -0,0 +1,16 @@
+package com.example.Cardapio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Indicando ao Spring que esta classe é um controlle
+@RestController
+//Indicando o endpoint que esa classe é responsável
+@RequestMapping("food")
+public class FoodController {
+
+    //Indicando ao spring quando o endpoint for chamado com o método get esta função é executada
+    public void getAll(){
+
+    }
+}
